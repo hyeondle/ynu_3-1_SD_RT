d573c4f103f13c0d11cd7634e5e6c37947be917b
hyeondle <st.linsio@gmail.com>
2024-06-03 00:53:29 +0900

    add class for object (sphere)

---
+++ include/Sphere.hpp
@@ -0,0 +1,17 @@
+#ifndef SPHERE_HPP
+# define SPHERE_HPP
+
+#include "./HitRecord.hpp"
+#include "./Vector.hpp"
+
+class sphere : public hittable {
+	public :
+		sphere(const point &center, double radius) : center(center), radius(fmax(0, radius)) {}
+		bool hit(const ray &r, double ray_tmin, double ray_tmax, hit_record &rec) const override;
+
+	private :
+		point center;
+		double radius;
+};
+
+#endif
---
+++ src/def/Sphere.cpp
@@ -0,0 +1,32 @@
+#include "../../include/Sphere.hpp"
+
+bool sphere::hit(const ray &r, double ray_tmin, double ray_tmax, hit_record &rec) const{
+	vector oc = center - r.origin();
+	auto a = r.direction().length_squared();
+	auto h = dot(r.direction(), oc);
+	auto c = oc.length_squared() - radius*radius;
+
+	auto discriminant = h*h - a*c;
+
+	if (discriminant < 0) {
+		return false;
+	}
+
+	auto sqrtd = sqrt(discriminant);
+
+	//find the nearest root that lies in the acceptable range
+	auto root = (h - sqrtd) / a;
+	if (root <= ray_tmin || ray_tmax <= root) {
+		root = (h + sqrtd) /a;
+		if (root <= ray_tmin || ray_tmax <= root) {
+			return false;
+		}
+	}
+
+	rec.t = root;
+	rec.p = r.at(rec.t);
+	vector outward_normal = (rec.p - center) / radius;
+	rec.set_face_normal(r, outward_normal);
+
+	return true;
+}
