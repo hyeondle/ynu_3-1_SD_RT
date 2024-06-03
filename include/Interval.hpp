975c10db17401bb434292d4c4bebcfd38eac44d7
hyeondle <st.linsio@gmail.com>
2024-06-03 14:59:40 +0900

    antialiasing added (but too slow)

--- include/Interval.hpp
+++ include/Interval.hpp
@@ -13,6 +13,7 @@ class interval {
 		double size() const;
 		bool contains(double x) const;
 		bool surrounds(double x) const;
+		double clamp(double x) const;
 
 		static const interval empty, universe;
 };
